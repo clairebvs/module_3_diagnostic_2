9c82183b9c8ccef84f52537d40bd7193474e68e2
ClaireBvs <clairembeauvais@gmail.com>
2018-10-17 11:12:39 -0600

    add stub mock for all_stations

--- spec/features/user_can_search_for_alt_station_spec.rb
+++ spec/features/user_can_search_for_alt_station_spec.rb
@@ -2,6 +2,10 @@ require 'rails_helper'
 
 feature "user can visit root page" do
   scenario "and fill the search form to find nearest alt station" do
+    file = File.open("./fixtures/all_stations.json")
+    stub_request(:get, "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?fuel_type=ELEC,LPG&location=80203&radius=6.0&limit=10")
+      .to_return(body: file, status: 200)
+
     visit "/"
 
     fill_in :q, with: 80203
